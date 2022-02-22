de0fbf1b544862ed7507f7398f0d59f7145c2555
emanfalola9998 <emmanuelfalola9998@gmail.com>
2022-02-22 17:02:18 +0000

    test being added

---
+++ src/find-shortest-string.test.js
@@ -0,0 +1,15 @@
+// import function from local file
+const findShortestString = require("./find-shortest-string");
+
+test("findshorteststring finds the shortest string in an array", () => {
+  expect(findShortestString(["hey", "is", "a", "exciting", "adventure"])).toBe(
+    "a"
+  );
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+});
+
+test("findshortest returns the earlier string in cases of joint shortest strings", () => {
+  expect(findShortestString(["short", "films"])).toBe("short");
+  expect(findShortestString(["song", "see", "zero", "sam"])).toBe("see");
+  expect(findShortestString(["luke", "john", "zara"])).toBe("luke");
+});
